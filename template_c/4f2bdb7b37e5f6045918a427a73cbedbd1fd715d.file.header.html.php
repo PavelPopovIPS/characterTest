64299cf4ad8cfe5e9479fb06bdf1c94241df9577
PavelPopovIPS <pavel.fargo@gmail.com>
2019-11-20 22:19:28 +0300

    добавил новые файлы

---
+++ template_c/4f2bdb7b37e5f6045918a427a73cbedbd1fd715d.file.header.html.php
@@ -0,0 +1,26 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2019-11-20 20:04:40
+         compiled from "D:\characterTest\template\header.html" */ ?>
+<?php /*%%SmartyHeaderCode:9251902215dd58e4822ee65-79314601%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '4f2bdb7b37e5f6045918a427a73cbedbd1fd715d' => 
+    array (
+      0 => 'D:\\characterTest\\template\\header.html',
+      1 => 1574107338,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '9251902215dd58e4822ee65-79314601',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_5dd58e48250d23_71650588',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5dd58e48250d23_71650588')) {function content_5dd58e48250d23_71650588($_smarty_tpl) {?><html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+  </head> 
+  <body><?php }} ?>
