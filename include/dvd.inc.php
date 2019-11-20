d85c84b771e7b465efba449fea0f71c40f08a411
PavelPopovIPS <pavel.fargo@gmail.com>
2019-11-20 22:47:06 +0300

    Почистил проект от лишних файлов и зависимостей

--- include/dvd.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function findDvd($title)
-    {
-        $title = dbQuote($title);
-        $query = "SELECT * FROM dvd
-                  WHERE title LIKE '%{$title}%'";
-        return dbQuery($query);
-    }
